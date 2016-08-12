379496a53ab9f19198f4a6b4f4a718625bb91d46
deisymsqr6@gmail.com <deisymsqr6@gmail.com>
2016-08-12 16:29:23 -0500

    Directory app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+AddressBook.create(name:"monica monsalve", email:"monica@gmail.com", telephone:3425033) # se ingresa datos por consola
+AddressBook.create(name:"carlos andres", email:"carlos24@gmail.com", telephone:3204455)
+AddressBook.create(name:"elizabeth blandon", email:"eliza23@gmail.com", telephone:5453421)
+AddressBook.create(name:"manuel correa", email:"manuel34@gmail.com", telephone:5323343)
+AddressBook.create(name:"carolina calle", email:"calle@gmail.com", telephone:4567788)
+AddressBook.create(name:"kevin perez", email:"kevin12@gmail.com", telephone:2345645)
+AddressBook.create(name:"silvia mesa", email:"silvia34@gmail.com", telephone:5142343)
+AddressBook.create(name:"belguin ruiz", email:"belguin4@gmail.com", telephone:3216788)
+AddressBook.create(name:"victor hugo", email:"victor@gmail.com", telephone:2124456)
+AddressBook.create(name:"juana perea", email:"juana@gmail.com", telephone:3809644)
+AddressBook.create(name:"jimena calle", email:"jime@gmail.com", telephone:5208976)
+AddressBook.create(name:"yarinton sannchez", email:"yarinton25@gmail.com", telephone:2345678)
